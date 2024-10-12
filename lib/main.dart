f964995fc11107f6964b99e033833ef26cd7cdf3
mehdinathani <mehdinathani@gmail.com>
2024-10-12 23:40:15 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:doctor_appointment_app/screens/onboarding_screen/onboarding_screen.dart';
+import 'package:doctor_appointment_app/screens/splash_screen/splash_screen.dart';
+import 'package:doctor_appointment_app/utils/themes/color_themes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Doctor Appointment App',
+        theme: ThemeData(
+          // textTheme: GoogleFonts.robotoSlabTextTheme(textTheme),
+
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a purple toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+          useMaterial3: true,
+        ),
+        home: FlutterSplashScreen(
+          duration: const Duration(milliseconds: 3000),
+          nextScreen: const OnboardingScreen(),
+          splashScreenBody: const SplashScreen(),
+          useImmersiveMode: true,
+        ));
+  }
+}
